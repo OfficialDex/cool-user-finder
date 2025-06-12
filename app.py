d3fb6481ceaf633aaae2f4dd998e9aa55e08c982
$ Blaze <140303092+OfficialDex@users.noreply.github.com>
2025-06-12 10:11:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "hi"
+
 @app.route('/login')
 def login():
     return render_template('home.html')
